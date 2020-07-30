67b32f2e3879a10b845d1982c85341c0f2650509
Adam Tatar <atatar2019@gmail.com>
2020-07-30 15:42:19 -0400

    added html card templates and built app functionality

--- Develop/app.js
+++ Develop/app.js
@@ -10,26 +10,142 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
+async function start(){
+    console.log("Build your team");
 
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
+    let teamHTML = "";
 
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
+    let teamSize;
 
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
+    await inquirer.prompt(
+        {
+            type: "number",
+            message: "How many people are in your team?",
+            name: "noOfTeamMem"
+        }
+    )
+    .then((data) => {
 
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
+        teamSize = data.noOfTeamMem + 1;
+    });
+    
+    if (teamSize === 0){
+        console.log("Nobody works here?");
+        return;
+    }
+    
+    for(x = 1; x < teamSize; x++){
 
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
+        // Global variables set
+        let name;
+        let id;
+        let title;
+        let email;
+
+        // Prompts user to answer the basic questions of the employee
+        await inquirer.prompt([ 
+            {
+                type: "input",
+                message: `What is employee (${x})'s name?`,
+                name: "name"
+            },
+            {
+                type: "input",
+                message: `What is the employee (${x})'s id?`,
+                name: "id"
+            },
+            {
+                type: "input",
+                message: `What is the employee (${x})'s Email?`,
+                name: "email"
+            },
+            {
+                type: "list",
+                message: `what the employee (${x})'s title?`,
+                name: "title",
+                choices: ["Engineer", "Intern", "Manager"]
+            }
+        ])
+        .then((data) => {
+
+            // Takes data from user and places value in global variables
+            name = data.name;
+            id = data.id;
+            title = data.title;
+            email = data.email;
+        });
+
+        // Switch Case depending on the title of the employee
+        switch (title){
+            case "Manager":
+
+                // ask user of Manager's Office Number
+                await inquirer.prompt([
+                    {
+                        type: "input",
+                        message: "What is your Manager's Office Number?",
+                        name: "officeNo"
+                    }
+                ])
+                .then((data) => {
+
+                    const manager = new Manager(name, id, email, data.officeNo);
+                    teamMember = fs.readFileSync("../templates/manager.html");
+                    teamHTML = teamHTML + "\n" + eval('`'+ teamMember +'`');
+                });
+                break;
+
+            
+            case "Intern":
+                await inquirer.prompt([
+                    {
+                        type: "input",
+                        message: "What school does you Intern go to?",
+                        name: "school"
+                    }
+                ])
+                .then((data) => {
+                    const intern = new Intern(name, id, email, data.school);
+                    teamMember = fs.readFileSync("../templates/intern.html");
+                    teamHTML = teamHTML + "\n" + eval('`'+ teamMember +'`');
+                });
+                break;
+
+            
+            case "Engineer":
+                await inquirer.prompt([
+                    {
+                        type: "input",
+                        message: "What is your Engineer's GitHub?",
+                        name: "github"
+                    }
+                ])
+                .then((data) => {
+                    const engineer = new Engineer(name, id, email, data.github);
+                    teamMember = fs.readFileSync("../templates/engineer.html");
+                    teamHTML = teamHTML + "\n" + eval('`'+ teamMember +'`');
+                });
+                break;
+
+        } 
+
+    }
+
+    
+    const mainHTML = fs.readFileSync("../templates/main.html");
+    teamHTML = eval('`'+ mainHTML +'`');
+
+    fs.writeFile("team.html", teamHTML, function(err) {
+
+        if (err) {
+          return console.log(err);
+        }
+      
+        console.log("Success!");
+      
+      });
+
+}
+
+
+start();
